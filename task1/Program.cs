6a2bcb12b8ce087810d9de479166c32effbca594
errasti <errasti007@yandex.ru>
2022-08-06 23:29:01 +0300

    added numbers info

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,11 +1,11 @@
 ﻿int[] getArray(int n)
 {
-    int [] arr = new int[n];
+    int[] arr = new int[n];
     Random rnd = new Random();
 
     for (int i = 0; i < n; i++)
     {
-        arr[i]=rnd.Next(-9,10);
+        arr[i] = rnd.Next(-9, 10);
     }
     return arr;
 }
@@ -13,7 +13,7 @@
 int[] array = getArray(12);
 int sumLess = 0;
 int sumGreat = 0;
-for ( int i = 0; i < array.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
     if (array[i] > 0)
     {
@@ -23,6 +23,7 @@ for ( int i = 0; i < array.Length; i++)
     {
         sumLess += array[i];
     }
+    Console.Write(array[i] + " ");
 }
-
+Console.WriteLine();
 Console.WriteLine("Сумма положительных - " + sumGreat + ", а сумма отрицательных - " + sumLess);
