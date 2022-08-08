8651099bac7b6123a25b78a1eb17ca114442e25b
errasti <errasti007@yandex.ru>
2022-08-08 23:38:38 +0300

    Initial commit

---
+++ task4/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] GetArray(int n)
+{
+    int[] arr = new int[n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rnd.Next(0, 1000);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите длину массива: ");
+int len = int.Parse(Console.ReadLine());
+int[] arr = GetArray(len);
+
+PrintArray(arr);
+
+int col = 0;
+for (int i = 0; i<arr.Length; i++)
+{
+    if(arr[i] >= 10 && arr[i] <= 99)
+    {
+        col += 1;
+    }
+}
+Console.WriteLine(col);
