631758d7e4df6e579629c68e395a8b03d10f2446
errasti <errasti007@yandex.ru>
2022-08-09 00:25:14 +0300

    Initial commit

---
+++ hw_01/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] CreateArray(int n)
+{
+    int[] arr = new int[n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rnd.Next(1,1000);
+    }
+    return arr;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int DigitCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Введите длину массива: ");
+int len = int.Parse(Console.ReadLine());
+int[] arr = CreateArray(len);
+
+ShowArray(arr);
+
+Console.WriteLine(DigitCount(arr));
+
